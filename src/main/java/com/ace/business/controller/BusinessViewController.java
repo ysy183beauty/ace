1dd33534f2d83183fe11307b19251048859c6b89
ysy_glb <ysy_glb@18388.com>
2020-11-08 17:01:08 +0800

    第四次提交

--- src/main/java/com/ace/business/controller/BusinessViewController.java
+++ src/main/java/com/ace/business/controller/BusinessViewController.java
@@ -1,23 +1,15 @@
 package com.ace.business.controller;
-
-import com.ace.business.entity.OperButton;
 import com.ace.common.controller.CommonController;
-import com.ace.sysytem.entity.BaseInfoSys;
-import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 @Controller
 @RequestMapping(value ="/business/view")
 public class BusinessViewController extends CommonController {
     @RequestMapping(value ="/selectBusinessInfo")
     public ModelAndView selectBusinessInfo(){
         ModelAndView mv=super.selectView("t_info");
-        mv.setViewName("/view/orgList");
+        mv.setViewName("/view/infoList");
         return  mv;
     }
 
